73651ddcbb4c5509c8c2d5b1e10771a3a9968bbb
steff880 <stefan.d.yordanov@gmail.com>
2021-02-18 11:40:09 +0000

    Add more content and finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ window.addEventListener('scroll', function () {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+function toggleMenu() {
+    const menuToggle = document.querySelector(".menuToggle");
+    const navigation = document.querySelector(".navigation");
+    menuToggle.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
